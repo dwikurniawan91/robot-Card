68b8b730a887b35ba3436e6713dffb7585616f91
dwikurniawan91 <dui.dict@gmail.com>
2019-06-26 15:09:29 +0700

    commit 1

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Dwi Kurniawan',
+      username: 'dwi',
+      email: 'duika@outlook.com'
+    },
+    {
+      id: 2,
+      name: 'Kirana Larasati',
+      username: 'fred',
+      email: 'fred@yahoo.com'
+    },
+    {
+      id: 3,
+      name: 'Enemonic',
+      username: 'Samantha',
+      email: 'elisa@sandei.net'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Harry',
+      username: 'Kamren',
+      email: 'harry@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
